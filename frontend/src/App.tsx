4d3f415d5b5b1fe7fa20879e2a3c6ab9f49bccda
Valentin <valentinspasov9669@gmail.com>
2022-02-24 23:25:22 +0200

    [Up] basic paging

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,47 @@
-import { FunctionComponent, useContext, useEffect } from 'react'
-import { CounrtiesContext } from './context/countries/CountriesContext'
+import { FunctionComponent, useCallback, useEffect, useMemo, useState } from 'react'
+import { Country } from './dto/Country'
 import * as CountriesService from './services/CountryService'
 
 interface Props {}
 
 export const App: FunctionComponent<Props> = props => {
-    const { counrties, setCounrties } = useContext(CounrtiesContext)
+    const [countries, setCounrties] = useState<Country[]>([])
+    const [page, setPage] = useState<number>(1)
+    const [size, setSize] = useState<number>(20)
+
+    const pageCount = useMemo<number>(
+        () => (countries.length % size === 0 ? countries.length / size : Math.floor(countries.length / size) + 1),
+        [countries.length, size]
+    )
+
+    const nextPageHandler = useCallback(() => {
+        setPage(page => page + 1)
+    }, [])
+
+    const prevPageHandler = useCallback(() => {
+        setPage(page => page - 1)
+    }, [])
 
     useEffect(() => {
         CountriesService.getAllCountries().then(setCounrties)
-    }, [setCounrties])
+    }, [])
 
-    return <div>{counrties}</div>
+    return (
+        <div>
+            <div>
+                <p>
+                    page: {page} - {pageCount}
+                </p>
+                <button disabled={page >= pageCount} onClick={nextPageHandler}>
+                    next
+                </button>
+                <button disabled={page <= 1} onClick={prevPageHandler}>
+                    prev
+                </button>
+            </div>
+            {countries.slice((page - 1) * size, (page - 1) * size + size).map(country => (
+                <div key={country?.capitalName + country.name}>{country.capitalName}</div>
+            ))}
+        </div>
+    )
 }
--- frontend/src/services/CountryService.ts
+++ frontend/src/services/CountryService.ts
@@ -1,12 +1,10 @@
 import { Country } from '../dto/Country'
 
-const basePath: string = process.env.REACT_APP_API_URL ?? ''
-
-const getResource = <T>(route: string): Promise<T> => {
-    return fetch(basePath + route)
-        .then(res => res.json())
-        .catch(console.log)
-}
-
 export const getAllCountries = (): Promise<Country[]> => getResource<Country[]>('/countries')
 export const getCountriesByName = (name: string): Promise<Country[]> => getResource<Country[]>(`/countries/${name}`)
+
+const getResource = <T>(route: string): Promise<T> => {
+    return fetch(process.env.REACT_APP_API_URL + route)
+        .then(res => res.json())
+        .catch(console.error)
+}

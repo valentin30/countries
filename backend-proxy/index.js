3e91f552f1ee1a1b5521656a7172f0447edcf32c
Valentin <valentinspasov9669@gmail.com>
2022-02-24 23:24:25 +0200

    [Up] proxy up and running

---
+++ backend-proxy/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const cors = require('cors')
+const express = require('express')
+const { createProxyMiddleware } = require('http-proxy-middleware')
+
+const HOST = 'localhost'
+const PORT = process.env.PORT
+const API_SERVICE_URL = process.env.API_SERVICE_URL
+
+const app = express()
+
+app.use(cors())
+
+app.get('/countries', createProxyMiddleware({ target: API_SERVICE_URL, changeOrigin: true }))
+
+app.listen(PORT, HOST, () => {})
